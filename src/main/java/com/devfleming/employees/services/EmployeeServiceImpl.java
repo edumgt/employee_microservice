2cffa623035d20eb7778a520fde297662b84c165
fleming99 <rfleming1235@gmail.com>
2024-11-03 21:47:21 -0300

    Changes in EmployeeServiceImpl: - Added JavaDoc in all methods; - Added @Cacheable, @CacheEvict and @CachePut in the methods; - Refactored the fetch methods to reduce lines of code and make more readable.

--- src/main/java/com/devfleming/employees/services/EmployeeServiceImpl.java
+++ src/main/java/com/devfleming/employees/services/EmployeeServiceImpl.java
@@ -9,9 +9,11 @@ import com.devfleming.employees.mappers.EmployeeMapper;
 import com.devfleming.employees.repositories.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,73 +26,107 @@ import java.util.Optional;
 public class EmployeeServiceImpl implements EmployeeService{
 
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
 
+    /**
+     * Responsible to create new employee.
+     * @param employeeDto Employee Data Transfer Object with the information of the employee that is going to be persisted.
+     * @return Employee entity that was created.
+     * @throws EmployeeAlreadyExistsException If the employee that it's going to be created has the same e-mail or cellphone of an existing employee.
+     */
     @Override
+    @CachePut(value = "createNewEmployeeCache", key = "#employeeDto.email")
     public Employee createNewEmployee(EmployeeDto employeeDto) {
 
-        Optional<Employee> employeeByEmail = Optional.ofNullable(employeeRepository.fetchEmployeeByEmail(employeeDto.getEmail()));
+        Optional<Employee> employeeByEmail = employeeRepository.fetchEmployeeByEmail(employeeDto.getEmail());
 
         if(employeeByEmail.isPresent()) throw new EmployeeAlreadyExistsException("The given e-mail is already in use");
 
-        Optional<Employee> employeeByCellphone = Optional.ofNullable(employeeRepository.fetchEmployeeByCellphone(employeeDto.getCellphoneNumber()));
+        Optional<Employee> employeeByCellphone = employeeRepository.fetchEmployeeByCellphone(employeeDto.getCellphoneNumber());
 
         if(employeeByCellphone.isPresent()) throw new EmployeeAlreadyExistsException("The given cellphone is already in use");
+
+        employeeDto.setActive('A');
 
         return employeeRepository.save(EmployeeMapper.mapToEmployee(employeeDto));
     }
 
+    /**
+     * Responsible to update an existing employee.
+     * @param employeeId Employee id that it's going to be updated.
+     * @param employeeDto Employee Data Transfer Object that holds the new information of the saved employee.
+     * @return Updated employee entity.
+     * @throws EmployeeNotFoundException If the employee doesn't exist.
+     * @throws RuntimeException If the employee is inactivated.
+     */
     @Override
+    @CachePut(value = "updateEmployeeCache", key = "#employeeId")
     public Employee updateEmployee(Long employeeId, EmployeeDto employeeDto) {
 
-        Optional<Employee> savedEmployee = employeeRepository.findById(employeeId);
+        Employee savedEmployee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new EmployeeNotFoundException("Did not found the employee by id: " + employeeId));
 
-        if (savedEmployee.isEmpty()) throw new EmployeeNotFoundException("Did not found the employee by id: " + employeeId);
+        if(savedEmployee.getActive().equals('I')) throw new RuntimeException("This employee is inactivated.");
 
-        Employee employeeToUpdate = savedEmployee.get();
+        savedEmployee.setFirstName(employeeDto.getFirstName());
+        savedEmployee.setLastName(employeeDto.getLastName());
+        savedEmployee.setCpf(employeeDto.getCpf());
+        savedEmployee.setRg(employeeDto.getRg());
+        savedEmployee.setRoleId(employeeDto.getRole());
+        savedEmployee.setEmail(employeeDto.getEmail());
+        savedEmployee.setAreaCode(employeeDto.getAreaCode());
+        savedEmployee.setCellphoneNumber(employeeDto.getCellphoneNumber());
 
-        employeeToUpdate.setFirstName(employeeDto.getFirstName());
-        employeeToUpdate.setLastName(employeeDto.getLastName());
-        employeeToUpdate.setCpf(employeeDto.getCpf());
-        employeeToUpdate.setRg(employeeDto.getRg());
-        employeeToUpdate.setRoleId(employeeDto.getRoleId());
-        employeeToUpdate.setEmail(employeeDto.getEmail());
-        employeeToUpdate.setAreaCode(employeeDto.getAreaCode());
-        employeeToUpdate.setCellphoneNumber(employeeDto.getCellphoneNumber());
-
-        return employeeRepository.save(employeeToUpdate);
+        return employeeRepository.save(savedEmployee);
     }
 
+    /**
+     * Responsible to fetch a single employee by its ID.
+     * @param employeeId Employee id that it's going to be fetched.
+     * @return Saved employee with the corresponding ID.
+     * @throws EmployeeNotFoundException If the employee doesn't exist with the corresponding ID.
+     */
     @Override
+    @Cacheable(value = "fetchEmployeeByIdCache", key = "#employeeId")
     public Employee fetchEmployeeById(Long employeeId) {
 
-        Optional<Employee> savedEmployee = employeeRepository.findById(employeeId);
-
-        if (savedEmployee.isEmpty()) throw new EmployeeNotFoundException("Did not found the employee with the id: " + employeeId);
-
-        return savedEmployee.get();
+        return employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new EmployeeNotFoundException("Did not found the employee by id: " + employeeId));
     }
 
+    /**
+     * Responsible to fetch a single employee by its e-mail.
+     * @param employeeEmail Employee e-mail that it's going to be fetched.
+     * @return Saved employee with the corresponding e-mail.
+     * @throws EmployeeNotFoundException If the employee doesn't exist with the corresponding e-mail.
+     */
     @Override
+    @Cacheable(value = "fetchEmployeeByEmailCache", key = "#employeeEmail")
     public Employee fetchEmployeeByEmail(String employeeEmail) {
 
-        Optional<Employee> savedEmployee = Optional.ofNullable(employeeRepository.fetchEmployeeByEmail(employeeEmail));
-
-        if (savedEmployee.isEmpty()) throw new EmployeeNotFoundException("Did not found the employee with the e-mail: " + employeeEmail);
-
-        return savedEmployee.get();
+        return employeeRepository.fetchEmployeeByEmail(employeeEmail)
+                .orElseThrow(() -> new EmployeeNotFoundException("Did not found the employee by e-mail: " + employeeEmail));
     }
 
+    /**
+     * Responsible to fetch a single employee by its cellphone.
+     * @param employeeCellphone Employee cellphone that it's going to be fetched.
+     * @return Saved employee with the corresponding cellphone.
+     * @throws EmployeeNotFoundException If the employee doesn't exist with the corresponding cellphone.
+     */
     @Override
+    @Cacheable(value = "fetchEmployeeByCellphoneCache", key = "#employeeCellphone")
     public Employee fetchEmployeeByCellphone(String employeeCellphone) {
 
-        Optional<Employee> savedEmployee = Optional.ofNullable(employeeRepository.fetchEmployeeByCellphone(employeeCellphone));
-
-        if (savedEmployee.isEmpty()) throw new EmployeeNotFoundException("Did not found the employee with the cellphone: " + employeeCellphone);
-
-        return savedEmployee.get();
+        return employeeRepository.fetchEmployeeByEmail(employeeCellphone)
+                .orElseThrow(() -> new EmployeeNotFoundException("Did not found the employee by cellphone: " + employeeCellphone));
     }
 
+    /**
+     * Responsible to fetch a list with all employees saved.
+     * @return The list of the employees.
+     * @throws EmployeeNotFoundException If there are no registered employees saved in database.
+     */
     @Override
     public List<Employee> fetchEmployeesList() {
 
@@ -101,14 +137,20 @@ public class EmployeeServiceImpl implements EmployeeService{
         return employeeList;
     }
 
+    /**
+     * Responsible to inactivate an employee.
+     * @param employeeId Employee ID that's going to be inactivated.
+     * @throws EmployeeNotFoundException If the employee doesn't exist with the corresponding ID.
+     * @throws RuntimeException If the employee is already inactive.
+     */
     @Override
+    @CacheEvict(value = "inactivateEmployeeCache", key = "#employeeId")
     public void inactivateEmployee(Long employeeId) {
 
-        Optional<Employee> savedEmployee = employeeRepository.findById(employeeId);
+        Employee employeeToInactivate = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new EmployeeNotFoundException("Did not found the employee with the id: " + employeeId));
 
-        if (savedEmployee.isEmpty()) throw new EmployeeNotFoundException("Did not found the employee with the id: " + employeeId);
-
-        Employee employeeToInactivate = savedEmployee.get();
+        if (employeeToInactivate.getActive().equals('I')) throw new RuntimeException("This employee is already inactive");
 
         employeeToInactivate.setActive('I');
 
