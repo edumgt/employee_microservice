eb98896a210191da33a5452b0b7db8b902ea324e
fleming99 <rfleming1235@gmail.com>
2024-11-10 21:37:17 -0300

    Added context test to assert that the repository and the service object is not null.

--- src/test/java/com/devfleming/employees/EmployeesApplicationTests.java
+++ src/test/java/com/devfleming/employees/EmployeesApplicationTests.java
@@ -1,13 +1,27 @@
 package com.devfleming.employees;
 
+import com.devfleming.employees.domain.usecases.EmployeeService;
+import com.devfleming.employees.repositories.EmployeeRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class EmployeesApplicationTests {
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Autowired
+	private EmployeeService employeeService;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(employeeRepository);
+		assertNotNull(employeeService);
 	}
 
 }
